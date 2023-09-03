77f5630dae98e6928b9530b11dd67a531e9982dc
coss <s_osss@163.com>
2023-09-04 02:34:05 +0800

    add fonts

--- install.sh
+++ install.sh
@@ -11,3 +11,13 @@ sudo make clean install && make clean
 
 cd ../slstatus/
 sudo make clean install && make clean
+
+cd ..
+
+sudo mkdir -p /usr/local/share/fonts && sudo cp -r FiraCode /usr/local/share/fonts/ && fc-cache
+
+cp -r ./config/VSCodium alacritty nvim picom rofi && cp -r ./config/Xresources ~/.Xresources && cp -r ./config/xinitrc ~/.xinitrc
+
+sudo pacman -S rofi alacritty
+
+
