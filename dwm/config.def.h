55bda710a82237e85b7930402c8f7f81a5661cbd
uan <i60888@163.com>
2023-09-23 22:20:02 +0800

    add keys for switching tags -adjacenttag

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -137,6 +137,10 @@ static const Key keys[] = {
 	{ MODKEY,            						XK_y,  	   togglescratch,  {.ui = 0 } },
 	{ MODKEY,            						XK_u,	     togglescratch,  {.ui = 1 } },
 	{ MODKEY,            						XK_x,	     togglescratch,  {.ui = 2 } },
+  { MODKEY,                       XK_Right,  viewnext,       {0} },
+	{ MODKEY,                       XK_Left,   viewprev,       {0} },
+	{ MODKEY|ShiftMask,             XK_Right,  tagtonext,      {0} },
+	{ MODKEY|ShiftMask,             XK_Left,   tagtoprev,      {0} },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
